ecdef6a1cdb1221e5345ae6049ddaf265cd9e87a
gareth-antle-wt <105876582+gareth-antle-wt@users.noreply.github.com>
2022-12-06 13:14:45 -0800

    Update day6.scala

--- day6.scala
+++ day6.scala
@@ -1,5 +1,3 @@
-package advent2022.day6
-
 import scala.collection.mutable
 
 object Day6 extends App {
@@ -14,28 +12,21 @@ object Day6 extends App {
     set.size == numUnique
   }
 
-  def findStartOfPacket(str: String): Int = {
-    for (x <- 0 until (str.length - 3)) {
-      if hasUniqueChars(str, x, 4) then return x
-    }
-    -1
-  }
-
-  def findStartOfMessage(string: String): Int = {
+  def findStartOfUniqueChunk(string: String, size: Int): Int = {
     for (x <- 0 until (string.length - 3)) {
-      if hasUniqueChars(string, x, 14) then return x
+      if hasUniqueChars(string, x, size) then return x + size
     }
     -1
   }
 
-  def part1() = {
-    println(findStartOfPacket(sequence) + 4)
+  def part1(): Unit = {
+    println(findStartOfUniqueChunk(sequence, 4))
   }
 
   part1()
 
-  def part2() = {
-    println(findStartOfMessage(sequence) + 14)
+  def part2(): Unit = {
+    println(findStartOfUniqueChunk(sequence, 14))
   }
 
   part2()
